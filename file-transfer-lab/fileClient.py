7b2a10c56a79872d0654c6e9e82a9a5f0383fa33
drincon2 <drincon2@miners.utep.edu>
2020-09-26 19:01:16 -0600

    Added file transfer from client side, currently fixing errors on client and server

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -2,6 +2,7 @@
 
 # Client
 import socket
+import sys
 
 # localhost and port
 host = '127.0.0.1'
@@ -9,8 +10,15 @@ port = 8080
 
 # Client socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+   # Connect to server
    s.connect((host, port))
-   s.sendall(b'Hello World!')
-   data = s.recv(1024)
+   # Open file
+   with open("fileTest.txt", "rb") as cf:
+      # Transfer file
+      tf = cf.read(1024)
+      for line in tf:
+         # Send file
+         data = s.send(bytes(str(line), 'utf-8')) 
    
 print('Received', repr(data))
+
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -9,13 +9,31 @@ port = 8080
 
 # Server socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+   # Connect to client 
    s.bind((host, port))
+   # Allow 1 client connection
    s.listen()
-   conn, addr = s.accept()
-   with conn:
-      print('Connected by', addr)
-      while True:
-         data = conn.recv(1024)
-         if not data:
-            break
-         conn.sendall(data)   
+   
+   # File receive loop
+   while True:
+      # Wait for incoming client connection
+      conn, addr = s.accept()
+      # File instance
+      fi = 1
+      with open("fileTest" + str(fi) + ".txt", "wb") as sf:
+         # Client transfer file
+         ctf = conn.recv(1024)
+         for line in ctf:
+            sf.write(bytes(str(line), 'utf-8'))
+         
+         print("File received!")
+         sf.close()
+      
+      print("Closing connection to client")
+      conn.close()
+
+print("Closing server socket")
+s.close()
+      
+            
+      

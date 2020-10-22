c28a9541035f57b0eaa272794d3dd1be6e205704
drincon2 <drincon2@miners.utep.edu>
2020-10-22 01:51:50 -0600

    Reimplemented framedSocket in fileClient and fileServer, and fixed send and receive errors. Reimplemented locks in fileServer

--- file-thread_transfer-lab/fileClient.py
+++ file-thread_transfer-lab/fileClient.py
@@ -5,69 +5,65 @@ import os
 # For params
 sys.path.append("../lib")
 import params
+# For framed socket 
+from framedSocket import FramedSock
+
 
 # client framework
-def client():
-   # Client switches
-   switchesVarDefaults = (
-       (('-s', '--server'), 'server', "127.0.0.1:50001"),
-       (('-d', '--debug'), "debug", False), # boolean (set if present)
-       (('-?', '--usage'), "usage", False), # boolean (set if present)
-       )
 
-   progname = "fileClient"
-   paramMap = params.parseParams(switchesVarDefaults)
-   server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+# Client switches
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "fileClient"
+paramMap = params.parseParams(switchesVarDefaults)
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
    
-   if usage:
-      params.usage()
+if usage:
+   params.usage()
       
-   # Client connection to server
-   # Split server into local host and port
-   try:
-      host, serverPort = re.split(":", server)
-      port = int(serverPort)
-   except:
-      print(">>Unable to parse port from '%s'" % server)
+# Client connection to server
+# Split server into local host and port
+try:
+   host, serverPort = re.split(":", server)
+   port = int(serverPort)
+except:
+   print(">>Unable to parse port from '%s'" % server)
+   sys.exit(1)
+# Make socket to connect and send files to server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+   # Check socket can be opened
+   if s is None:
+      print(">>Socket could not be opened")
       sys.exit(1)
-   # Make socket to connect and send files to server
-   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-      # Check socket can be opened
-      if s is None:
-         print(">>Socket could not be opened")
-         sys.exit(1)
-      # Connect to server
-      s.connect((host, port))
-      
-      # Send files 
-      while True:
-         usr_in = input(">>Please type the file you want to upload to server.\n" +
-                        ">>Alternatively, if you wish to close connection to server, type 'exit'.\n>>")
-         # Close connection to server
-         if usr_in == "exit":
-            sys.exit(0)
-         # Send file to server
+   # Connect to server
+   s.connect((host, port))
+   
+   # Make new client framed socket
+   encap_s = FramedSock((s, (host, port)))
+   
+   # Send files 
+   while True:
+      usr_in = input(">>Please type the file you want to upload to server.\n" +
+                     ">>Alternatively, if you wish to close connection to server, type 'exit'.\n>>")
+      # Close connection to server
+      if usr_in == "exit":
+         sys.exit(0)
+      # Send file to server
+      else:
+         # Check file exists in current directory
+         filename = usr_in
+         if os.path.exists(filename):   
+            # Send file data. Errors might happen here
+            file = open(filename, "rb")
+            file_data = file.read()
+            encap_s.framedSend(filename, file_data, debug)
+            
+            print(">>File sent")
+            
          else:
-            # Check file exists in current directory
-            filename = usr_in
-            if os.path.exists(filename):   
-               # Send name of file
-               s.send(filename.encode())
+            print("File %s not found" % filename) 
 
-               # Send file data
-               with open(filename, "r") as cf:
-                  while True:
-                     # Transfer file
-                     tf = cf.read(1024)
-                     # Send all file data to server
-                     s.send(tf.encode())
-                     if not tf:
-                        break
-               cf.close()
-            else:
-               print("File %s not found" % filename) 
-
-   print('>>Received', repr(data))
-   s.close() 
-
-client()
--- file-thread_transfer-lab/fileServer.py
+++ file-thread_transfer-lab/fileServer.py
@@ -3,72 +3,104 @@
 import sys, re, socket, os
 import threading
 # For params
-sys.path.append("../lib")       
+sys.path.append("../lib")
 import params
+# For framed socket
+from framedSocket import FramedSock
 
+# Lock
+lock = threading.Lock()
+# Server files
+files = list()
 
-# Handle client connection and file transfer
-def handle_client(conn, addr):
-   # Display client host and port
-   print(f">>New connection: {addr} connected.")
-   fi = 0
-   fi += 1
-   # Get name of file
-   filename = conn.recv(1024).decode()
-   print(filename)
+# Server framework
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
 
-   # Save client file
-   with conn:
-      while True:
-         data = conn.recv(1024)
-         udata = data.decode()
-         if udata:
-            with open(str(fi) + filename, "w") as fp:
-               for line in udata:
-                  fp.write(line)
-            fp.close()
-            print(f">>File received from client {addr}!")
-         elif not data:
-            break
-   print(f">>Closing connection to client {addr}")
-   conn.close()
+progname = "fileServer"
+paramMap = params.parseParams(switchesVarDefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+   params.usage()
+
+# Server connection to client
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+bindAddr = ("127.0.0.1", listenPort)
+try:
+   s.bind(bindAddr)
+except socket.error as msg:
+   print(">>Bind failed. Error code : " + str(msg[0]) + " Message " + msg[1])
+   s.close()
+s.listen()
+print(f">>Server listening on: {bindAddr}")
+
+# Server file transfer
+class Server(threading.Thread):
+   def __init__(self, s):
+      threading.Thread.__init__(self)
+      self.sock, self.addr = s
+      self.file_s = FramedSock(s)
+
+   # Check file availability
+   # Apply lock to ensure only one thread checks file availability at a time
+   def file_transfer_start(self, file_name):
+      global lock, files
+      lock.acquire()
+      if file_name in files:
+         lock.release()
+         print("File currently in use. Try again later. Closing connection...")
+         sys.exit(1)
+      else:
+         files.append(file_name)
+         lock.release()
+
+   # Close file
+   # Apply lock to ensure only one thread removes the file it was using
+   def file_transfer_end(self, file_name):
+      global lock, files
+      lock.acquire()
+      files.remove(file_name)
+      lock.release()
+
+   # Save client file to server
+   def save_file(self, file_name, file_data):
+      file_name = file_name.decode()
+      if not os.path.exists("./Server_Files/" + file_name):
+         # Check file availability and apply lock
+         self.file_transfer_start(file_name)
+         # Save client file in server
+         with open("./Server_Files/" + file_name, "w+b") as save_file:
+            save_file.write(file_data)
+         save_file.close()
+         print(f">>File received from client {self.addr}!")
+         self.file_transfer_end(file_name)
+      else:
+         print(f">>Filename {file_name} already exists in server. Closing connection...")
+         sys.exit(1)
+      
+   # Handle client
+   def run(self):
+      # Display client host and port
+      print(f">>New connection: {self.addr} connected.")
+      # Get name of file
+      file_name, file_data = self.file_s.framedReceive(debug)
+      # Get file size
+      if debug: print("rec'd: ", payload)
+      # Check file is not empty
+      if file_data is None:
+         print(">>File is empty. Closing connection...")
+         sys.exit(1)
+      # Save client file to server
+      self.save_file(file_name, file_data)
+      
    
 
-# Start client connection 
-def start(s):
-   # Allow as many connections as needed
-   s.listen()
-   print(f">>Server is listening on 127.0.0.1")
-   # Start a new thread for each new client connection
-   while True:
-      conn, addr = s.accept()
-      thread = threading.Thread(target=handle_client, args=(conn, addr))
-      thread.start()
-      print(f">>Active connections: {threading.activeCount() -1}")
-      
-
-# Server framework    
-def server():
-   switchesVarDefaults = (
-       (('-l', '--listenPort') ,'listenPort', 50001),
-       (('-d', '--debug'), "debug", False), # boolean (set if present)
-       (('-?', '--usage'), "usage", False), # boolean (set if present)
-       )
-   progname = "fileServer"
-   paramMap = params.parseParams(switchesVarDefaults)
-
-   debug, listenPort = paramMap['debug'], paramMap['listenPort']
-   if paramMap['usage']:
-      params.usage()
-    
-   # Server connection to client
-   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-      try:
-         s.bind(("127.0.0.1", listenPort))
-      except socket.error as msg:
-         print(">>Bind failed. Error code : " + str(msg[0]) + " Message " + msg[1])
-         s.close()
-      # Start client connection   
-      start(s)
-
-server()
+while True:
+   sock = s.accept()
+   server = Server(sock)
+   server.start()
